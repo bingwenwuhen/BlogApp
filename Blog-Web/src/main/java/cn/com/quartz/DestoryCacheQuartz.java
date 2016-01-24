882361d6b23728071e51911b88b94bf6e1f0814f
bingwenwuhen <bingwenwuhen@163.com>
2016-01-24 21:17:33 +0800

    增加quartz，准备编写一个月后替换缓存的class类

---
+++ Blog-Web/src/main/java/cn/com/quartz/DestoryCacheQuartz.java
@@ -0,0 +1,15 @@
+package cn.com.quartz;
+
+import javax.annotation.PreDestroy;
+
+/**
+ * Created by Administrator on 2016/1/24.
+ */
+public class DestoryCacheQuartz {
+
+
+    @PreDestroy
+    public void destory() {
+
+    }
+}
