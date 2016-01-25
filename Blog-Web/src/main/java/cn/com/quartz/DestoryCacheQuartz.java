29180da4f7b26415c9e89b681cdf669b0defd17f
xiaxuan <xiaxuan@haizhi.com>
2016-01-25 15:44:58 +0800

    定时器配置完成

--- Blog-Web/src/main/java/cn/com/quartz/DestoryCacheQuartz.java
+++ Blog-Web/src/main/java/cn/com/quartz/DestoryCacheQuartz.java
@@ -1,11 +1,19 @@
 package cn.com.quartz;
 
+import lombok.extern.slf4j.Slf4j;
+
 import javax.annotation.PreDestroy;
 
 /**
  * Created by Administrator on 2016/1/24.
  */
+@Slf4j
 public class DestoryCacheQuartz {
+
+
+    public void run() {
+
+    }
 
 
     @PreDestroy
