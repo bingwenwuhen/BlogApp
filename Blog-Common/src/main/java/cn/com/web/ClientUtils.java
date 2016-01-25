33979967eea0b1fd226b21f5464ccc0085a0be76
bingwenwuhen <bingwenwuhen@163.com>
2016-01-25 23:50:57 +0800

    将client改为单例的

--- Blog-Common/src/main/java/cn/com/web/ClientUtils.java
+++ Blog-Common/src/main/java/cn/com/web/ClientUtils.java
@@ -13,6 +13,9 @@ public class ClientUtils {
     private static Client client;
 
     public static Client getClient() {
+        if (client == null) {
+            client = new Client();
+        }
         return client;
     }
 }
