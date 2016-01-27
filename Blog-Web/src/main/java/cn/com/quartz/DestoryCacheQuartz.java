a295559ce22aa11b8d0c343b1df82f12d3da45fb
xiaxuan <xiaxuan@haizhi.com>
2016-01-27 13:53:18 +0800

    编写刷新缓存的类

--- Blog-Web/src/main/java/cn/com/quartz/DestoryCacheQuartz.java
+++ Blog-Web/src/main/java/cn/com/quartz/DestoryCacheQuartz.java
@@ -1,8 +1,12 @@
 package cn.com.quartz;
 
 import lombok.extern.slf4j.Slf4j;
+import org.mongodb.morphia.Datastore;
+import redis.clients.jedis.ShardedJedis;
+import redis.clients.jedis.ShardedJedisPool;
 
 import javax.annotation.PreDestroy;
+import javax.inject.Inject;
 
 /**
  * Created by Administrator on 2016/1/24.
@@ -10,11 +14,23 @@ import javax.annotation.PreDestroy;
 @Slf4j
 public class DestoryCacheQuartz {
 
+    @Inject
+    private ShardedJedisPool jedisPool;
 
+    @Inject
+    private Datastore datastore;
     /**
      * 在此处定时刷新缓存
      */
     public void run() {
+        ShardedJedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+        } catch (Exception e) {
+            jedis.close();
+            log.error("error:{}", e);
+        }
+
 
     }
 
