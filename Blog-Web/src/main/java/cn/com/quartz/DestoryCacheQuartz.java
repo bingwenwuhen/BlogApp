d1637a0a894ca16359038a639645957a262fdd83
bingwenwuhen <bingwenwuhen@163.com>
2016-01-28 23:07:28 +0800

    基本完成定时器的定时更新缓存和删除缓存的代码

--- Blog-Web/src/main/java/cn/com/quartz/DestoryCacheQuartz.java
+++ Blog-Web/src/main/java/cn/com/quartz/DestoryCacheQuartz.java
@@ -1,14 +1,21 @@
 package cn.com.quartz;
 
+import cn.com.container.ServiceProvinder;
+import cn.com.domain.Article;
+import cn.com.domain.ArticleMeta;
 import cn.com.service.ArticleMetaService;
 import cn.com.service.ArticleService;
 import lombok.extern.slf4j.Slf4j;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.mongodb.morphia.Datastore;
 import redis.clients.jedis.ShardedJedis;
 import redis.clients.jedis.ShardedJedisPool;
 
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Administrator on 2016/1/24.
@@ -18,9 +25,6 @@ public class DestoryCacheQuartz {
 
     @Inject
     private ShardedJedisPool jedisPool;
-
-    @Inject
-    private Datastore datastore;
 
     @Inject
     private ArticleService articleService;
@@ -52,6 +56,21 @@ public class DestoryCacheQuartz {
      */
     private void DestoryCace(ShardedJedis jedis) {
         log.info("删除cache");
+        List<ArticleMeta> articleMetas = null;
+        try {
+            articleMetas = new ObjectMapper().readValue(jedis.get("RecentlyArticles"), ArrayList.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //将这个key删除
+        if (articleMetas != null) {
+            jedis.del("RecentlyArticles");
+            for (ArticleMeta articleMeta: articleMetas) {
+                //将文章元数据与文章均删除
+                jedis.del(articleMeta.getId().toString());
+                jedis.del(articleMeta.getArticleId());
+            }
+        }
     }
 
     /**
@@ -60,6 +79,27 @@ public class DestoryCacheQuartz {
      */
     private void updateCache(ShardedJedis jedis) {
         log.info("更新cache");
+        List<ArticleMeta> articleMetas = articleMetaService.loadArticleRecently();
+        try {
+            if (articleMetas != null && articleMetas.size() > 0) {
+                jedis.set("RecentlyArticles", new ObjectMapper().writeValueAsString(articleMetas));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            log.error("error:{}", e);
+        }
+        if (articleMetas != null &&  articleMetas.size() > 0) {
+            for (ArticleMeta articleMeta: articleMetas) {
+                Article article = articleService.loadArticle(articleMeta.getArticleId());
+                //将文章元数据信息以及文章均放到缓存中
+                try {
+                    jedis.set(articleMeta.getId().toString(), new ObjectMapper().writeValueAsString(articleMeta));
+                    jedis.set(article.getId(), new ObjectMapper().writeValueAsString(article));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
 }
