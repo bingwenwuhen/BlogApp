2e3efdc505a950b7fe1e7c0e88b9dc9fdb580ceb
bingwenwuhen <bingwenwuhen@163.com>
2016-01-28 23:12:11 +0800

    编写FileController用来上传下载图片

---
+++ Blog-Web/src/main/java/cn/com/controller/FileController.java
@@ -0,0 +1,11 @@
+package cn.com.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Administrator on 2016/1/28.
+ */
+@Controller
+public class FileController {
+
+}
